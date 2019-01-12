7cb218857e3395fbfe4a21eed39de5f907a351a9
YMandarin <46614277+YMandarin@users.noreply.github.com>
2019-01-12 23:13:27 +0100

    Big TextAdd

--- Main.py
+++ Main.py
@@ -1,25 +1,39 @@
 import pygame
 
-size = 200
+size = 600
 alter = int(size * 0.2)
 
 pygame.init()
+pygame.font.init()
 pygame.display.set_caption("TikTakToe")
 canvas = pygame.display.set_mode((size, size + alter))
 clock = pygame.time.Clock()
 
 blockSize = int(size / 3)
 
+cross = pygame.image.load("Cross.png")
+circle = pygame.image.load("Circle.png")
+
+firstCross = pygame.transform.scale(cross, (blockSize, blockSize))
+firstCircle = pygame.transform.scale(circle, (blockSize, blockSize))
+
 Ways = {
     (0, 0): [[(1, 0), (2, 0)], [(0, 1), (0, 2)], [(1, 1), (2, 2)]],
     (1, 0): [[(0, 0), (2, 0)], [(1, 1), (1, 2)]],
     (2, 0): [[(0, 0), (1, 0)], [(2, 1), (2, 2)], [(1, 1), (0, 2)]],
-    (0, 1): [[(1, 1), (1, 2)], [(0, 0), (0, 2)]],
+    (0, 1): [[(1, 1), (2, 1)], [(0, 0), (0, 2)]],
     (1, 1): [[(0, 0), (2, 2)], [(2, 0), (0, 2)], [(1, 0), (1, 2)], [(0, 1), (2, 1)]],
     (2, 1): [[(0, 1), (1, 1)], [(2, 0), (2, 2)]],
     (0, 2): [[(0, 0), (0, 1)], [(1, 2), (2, 2)], [(1, 1), (2, 0)]],
     (1, 2): [[(0, 2), (2, 2)], [(1, 0), (1, 1)]],
     (2, 2): [[(2, 0), (2, 1)], [(0, 2), (1, 2)], [(0, 0), (1, 1)]],
+}
+
+Colors = {
+    "Black": (0, 0, 0),
+    "White": (255, 255, 255),
+    "LightGray": (200, 200, 200),
+    "DarkGray": (100, 100, 100)
 }
 
 
@@ -56,11 +70,59 @@ class Block:
 
     def draw(self):
         if self.type == 1:
-            pygame.draw.circle(canvas, (0, 0, 0),
-                               (self.x * blockSize + int(blockSize / 2), self.y * blockSize + int(blockSize / 2),),
-                               int(blockSize / 2))
+            canvas.blit(firstCircle, (self.x * blockSize, self.y * blockSize))
         else:
-            pygame.draw.rect(canvas, (0, 0, 0), (self.x * blockSize, self.y * blockSize, blockSize, blockSize))
+            canvas.blit(firstCross, (self.x * blockSize, self.y * blockSize))
+
+
+class Text:
+
+    def __init__(self, Text, pos, size = 50,color = Colors["Black"], Font="tahoma", bold = False, italic = False):
+        self.text = Text
+        self.pos = pos
+        self.font = Font
+        self.size = size
+        self.bold = bold
+        self.italic = italic
+        self.color = color
+        self.Font = pygame.font.SysFont(self.font, self.size, self.bold, self.italic)
+        self.Text = self.Font.render(self.text, True, self.color)
+        self.surface = self.Text.get_rect()
+
+
+
+    def getSize(self):
+        return self.surface
+
+    def setText(self,Text,size = -1,color = -1,Font = -1):
+
+        self.text = Text
+        if not size == -1:
+            self.size = size
+        if not color == -1:
+            self.color = color
+        if not Font == -1:
+            self.font = Font
+
+
+        self.render()
+
+    def setPos(self,pos):
+        self.pos = pos
+        self.render()
+
+    def draw(self,surface = canvas):
+        x = self.pos[0]
+        y = self.pos[1]
+        w = self.surface[0]
+        h = self.surface[1]
+
+        surface.blit(self.Text,(x - w//2,y-h//2))
+
+    def render(self):
+        self.Font = pygame.font.SysFont(self.font, self.size, self.bold, self.italic)
+        self.Text = self.Font.render(self.text, True, self.color)
+        self.surface = self.Text.get_rect()
 
 
 Map = []
@@ -96,18 +158,23 @@ def check(x, y, type):
 
 Type = 0
 won = False
+reset = False
+
 run = True
+
+width = int(alter * 0.5)
+secondCross = pygame.transform.scale(cross, (width, width))
+secondCircle = pygame.transform.scale(circle, (width, width))
 
 
 def draw():
-    canvas.fill((255, 255, 255))
-    pygame.draw.rect(canvas, (200, 200, 200), (0, size, size, alter))
+    canvas.fill(Colors["White"])
+    pygame.draw.rect(canvas, Colors["DarkGray"], (0, size, size, alter))
 
-    width = int(alter * 0.4)
     if Type == 1:
-        pygame.draw.circle(canvas, (0, 0, 0), (int(size * 0.8) - width//2, size + int(alter / 2) - width), int(width/2))
+        canvas.blit(secondCircle, (int(size * 0.85) - width // 2, size + alter // 2 - width // 2))
     else:
-        pygame.draw.rect(canvas, (0, 0, 0), (int(size * 0.8) - width//2, size + int(alter / 2) - width,width,width))
+        canvas.blit(secondCross, (int(size * 0.85) - width // 2, size + alter // 2 - width // 2))
 
     for x in Map:
         for i in x:
@@ -128,6 +195,13 @@ while run:
                 Map[x2][i2] = 0
         won = False
 
+    elif reset:
+        pygame.time.delay(50)
+        for x2 in range(0, 3):
+            for i2 in range(0, 3):
+                Map[x2][i2] = 0
+        reset = False
+
     e = pygame.event.get()
     for event in e:
         if event.type == pygame.QUIT:
@@ -145,7 +219,7 @@ while run:
             run = False
 
         if leftcl and event.type == pygame.MOUSEBUTTONDOWN:
-            if not isinstance( Map[mouse[0]][mouse[1]],Block):
+            if not isinstance(Map[mouse[0]][mouse[1]], Block):
                 Map[mouse[0]][mouse[1]] = Block(Type, mouse[0], mouse[1])
 
                 if check(mouse[0], mouse[1], Type):
@@ -156,7 +230,13 @@ while run:
                 else:
                     Type = 0
 
-    draw()
+                if not won:
+                    reset = True
+                    for x in Map:
+                        for i in x:
+                            if i == 0:
+                                reset = False
 
+    draw()
 
 pygame.quit()
